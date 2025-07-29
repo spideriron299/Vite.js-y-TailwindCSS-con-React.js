753f72daa1e4b05ea36b480fe46f3ddbe67954d0
Diego Badillo <diegobadillo@live.com>
2025-07-28 20:18:03 -0600

    Clase 14

--- src/Components/Context/index.jsx
+++ src/Components/Context/index.jsx
@@ -3,10 +3,16 @@ import { createContext, useState } from 'react';
 export const ShoppingCartContext = createContext()
 
 export const ShoppingCartProvider = ({ children }) => {
+    //Shopping Cart Increment Quantity
     const [count, setCount] = useState(0)
+
+    //Product Detail Open/Close
     const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
     const openProductDetail = () => setIsProductDetailOpen(true)
     const closeProductDetail = () => setIsProductDetailOpen(false)
+
+    // Product Detail Show Product
+    const [productToShow, setProductToShow] = useState({})
 
 
     console.log('COUNT: ', count)
@@ -16,7 +22,9 @@ export const ShoppingCartProvider = ({ children }) => {
             setCount,
             openProductDetail,
             closeProductDetail,
-            isProductDetailOpen
+            isProductDetailOpen,
+            productToShow,
+            setProductToShow
         }}>
             {children}
         </ShoppingCartContext.Provider>
