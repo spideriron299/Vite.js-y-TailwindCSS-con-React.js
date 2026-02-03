8766ebb652ce7bda98a7e451931e660a10122801
DiegoBadillo <diegoBadillo@live.com>
2026-02-03 02:55:37 -0600

    Clase 23 Pagina individual de cada orden

--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -1,11 +1,27 @@
+import { useContext } from 'react'
+import { Link } from 'react-router-dom'
 import Layout from '../../Components/Layout'
+import { ShoppingCartContext } from '../../Components/Context'
+import  OrdersCard  from '../../Components/OrdersCard'
 
 function MyOrders() {
+    const context = useContext(ShoppingCartContext)
 
   return (
     <Layout>
-      MyOrders
-    </Layout>
+      <div className='flex items-center justify-center relative w-80'>
+        <h1>My Orders</h1>
+      </div>
+      {
+        context.order.map((order, index) => {
+          <Link to={`/my-orders/${order.id}`}>
+            <OrdersCard  
+              totalPrice={order.totalPrice} 
+              totalProducts={order.totalProducts}/>
+          </Link>
+        })
+      }
+    </Layout> 
   )
 }
 
