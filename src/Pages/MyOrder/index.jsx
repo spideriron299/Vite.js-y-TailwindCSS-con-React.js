de978f0269db9661b59e9ba2d4b4324c35fe5e34
DiegoBadillo <diegoBadillo@live.com>
2026-02-10 20:04:47 -0600

    Clase 24 Ordenes indivudales

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -7,7 +7,9 @@ import Layout from '../../Components/Layout'
 
 function MyOrder() {
   const context = useContext(ShoppingCartContext)
-
+  const currentPath = window.location.pathname
+  let index = currentPath.substring(currentPath.lastIndexOf('/') + 1)
+  if (index == 'last') index = context.order?.length - 1 
   return (
     <Layout>
       <div className='flex items-center justify-center relative w-80 mb-6'>
@@ -18,7 +20,7 @@ function MyOrder() {
       </div>
       <div className='flex-col w-80'>
       {
-      context.order?.slice(-1)[0].products.map(product =>(
+      context.order?.[index]?.products.map(product =>(
           <OrderCard
               key={product.id}
               id={product.id}
